6690d699c190d785048c7e15b3e931d19ffe2909
loganperreault <perreault.logan@gmail.com>
2015-04-23 09:47:58 -0600

    More analysis.

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -1,3 +1,5 @@
+models <- c("NaiveBayes","NeuralNet","SVM","LogisticRegression","KNearest","MixtureModel")
+
 getData <- function(directory) {
   models <- c("NaiveBayes","NeuralNet","SVM","LogisticRegression","KNearest","MixtureModel")
   datasets <- list()
@@ -62,4 +64,43 @@ plotIndCheck <- function(directory, dataset, title = NULL) {
     title <- paste("PC", dataset, " Residuals", sep="")
   expected <- rep(means, each=nrow(resid))
   plot(expected, residuals, main = title)
+}
+
+formatDataForTest <- function(directory, dataset) {
+  pc <- getPCData(directory)
+  df <- pc[[dataset]]
+  response <- convertToVector(df)
+  alternative <- rep(1:6, each=10)
+  data = data.frame(response = response, alternative = factor(alternative))
+}
+
+plotAnovaTest <- function(directory, dataset, title = NULL) {
+  data <- formatDataForTest(directory, dataset)
+  aov.fit <- aov(data$response ~ data$alternative)
+  if (is.null(title))
+    title <- paste("PC", dataset, " Residuals", sep="")
+  plot(aov.fit, main = title)
+}
+
+anovaTest <- function(directory, dataset) {
+  data <- formatDataForTest(directory, dataset)
+  aov.fit <- aov(data$response ~ data$alternative)
+  summary(aov.fit)
+}
+
+tukeyTest <- function(directory, dataset) {
+  data <- formatDataForTest(directory, dataset)
+  aov.fit <- aov(data$response ~ data$alternative)
+  cat(as.numeric(TukeyHSD(aov.fit)[[1]][,4]), sep="\n")
+}
+
+plotMeanAccuracy <- function() {
+  models <- c("NaiveBayes","NeuralNet","SVM","LogisticRegression","KNearest","MixtureModel")
+  accuracy <- c(0.8790284,0.90342864,0.91711522,0.92266538,0.91656084,0.32172642)
+  f1 <- c(0.93260964,0.93940994,0.95590466,0.95901562,0.9553374,0.106718608)
+  plotdata <- matrix(c(accuracy,f1),nrow=2,byrow=TRUE)
+  colnames(plotdata) <- models
+  rownames(plotdata) <- c("Accuracy","F1")
+  barplot(plotdata, xlab="Classifier", legend = rownames(plotdata), beside=TRUE)
+  
 }
